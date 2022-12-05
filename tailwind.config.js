ecf081e8061e9362fddf604425efb6d76659444a
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2022-12-05 09:53:35 +0500

    Just get started

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  content: [],
-  theme: {
-    extend: {},
+  future: {
+    removeDeprecatedGapUtilities: true,
   },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        'xs': {
+            'max': '767px'
+        }
+    },
+      colors: {
+        'accent-1': '#333',
+      },
+    },
+  },
+  variants: {},
   plugins: [],
+
 }
