d4eb4692248c026088afff84444f7607029193b5
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2023-01-03 21:24:03 +0500

    Home Components
    Languages component done.

--- components/home/HeroSec.jsx
+++ components/home/HeroSec.jsx
@@ -2,12 +2,38 @@ import React from "react";
 
 const HeroSec = () => {
     return(
-        <section className="heroSec bg-black pt-16 pb-16 px-5 xl:px-0">
+        <section className="heroSec bg-[#282A35] pt-16 pb-16 px-5 xl:px-0">
 
-            <div className="max-w-1140px mx-auto text-center py-4">
-                <div className="container mx-auto">
-                    <h1 className="text-4xl md:text-5xl lg:text-7xl text-white font-roboto font-black mb-10">Learn to code by Practice</h1>
+            <div className="container mx-auto text-center py-4">
+                <div className="">
+                    <h1 className="text-4xl md:text-[70px] lg:text-7xl text-white font-roboto font-black mb-10">Learn to code by Practice</h1>
                     <p className="text-white">With the world's largest web developer site.</p>
+                </div>
+
+                <div className="flex items-center">
+                    <div className="flex space-x-1">
+                        <input
+                            type="text"
+                            className="block w-full px-4 py-2 text-purple-700 bg-white border rounded-full focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                            placeholder="Search..."
+                        />
+                        <button className="px-4 text-white bg-purple-600 rounded-full ">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-5 h-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                strokeWidth={2}
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                />
+                            </svg>
+                        </button>
+                    </div>
                 </div>
             </div>
 
--- components/home/languages.jsx
+++ components/home/languages.jsx
@@ -1,19 +1,57 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 
 
 const Languages = () => {
     return(
-        <section className="languages">
-            <div className="container mx-auto">
-                <div className="content_wrapper">
-                    <div className="ref__links">
-                        <h2 className="text-7xl">html</h2>
-                        <p>The language for building web pages</p>
-                        <div className="button___link">
-                            <Link href="/">
-                                Learn HTML
+        <section className="languages px-[15px] xl:px-0 py-20 bg-[#D9EEE1]">
+            <div className="container max-w-6xl mx-auto">
+                <div className="content_wrapper flex">
+                    <div className="ref__links mr-20">
+                        <h2 className="text-[100px] text-center font-bold uppercase">html</h2>
+                        <p className="text-[19px] text-center text-black font-normal">The language for building web pages</p>
+
+                        <div className="btn____link my-7">
+                            
+                            <div className="button___link bg-[#04AA6D] inline-flex items-center justify-center w-[200px] h-[43px] border border-[#04AA6D] rounded-full mr-5">
+                                <Link href="/" className="text-[18px] text-white font-normal">
+                                    Learn HTML
+                                </Link>
+                            </div>
+
+                            <div className="button___link bg-[#FFF4A3] inline-flex items-center justify-center w-[200px] h-[43px] border border-[#FFF4A3] rounded-full">
+                                <Link href="/" className="text-[18px] text-black font-normal">
+                                    Watch tutorial
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className="btn____links">
+                            <div className="button___link bg-[#282A35] inline-flex items-center justify-center w-[200px] h-[43px] border border-[#282A35] rounded-full mr-5">
+                                <Link href="/" className="text-[18px] text-white font-normal">
+                                    HTML Reference
+                                </Link>
+                            </div>
+
+                            <div className="button___link bg-[#FFC0C7] inline-flex items-center justify-center w-[200px] h-[43px] border border-[#FFC0C7] rounded-full">
+                                <Link href="/" className="text-[18px] text-black font-normal">
+                                    Get Certified
+                                </Link>
+                            </div> 
+                        </div>  
+                    </div>
+
+                    <div className="html-example p-[16px] bg-[#E7E9EB] max-w-[615px] rounded-md">
+                        <h3 className="text-[24px] text-black font-semibold uppercase">html example:</h3>
+
+                        <div className="example bg-white w-[580px] h-[300px] my-4">
+
+                        </div>
+                        <div className="button___link bg-[#04AA6D] inline-flex items-center justify-center w-[200px] h-[43px] border border-[#04AA6D] rounded-full mr-5">
+                            <Link href="/" className="text-[18px] text-white font-normal">
+                                Try it Yourself
                             </Link>
                         </div>
                     </div>
