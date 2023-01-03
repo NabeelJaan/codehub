589a6e95710a3e082f12759601158aba8528627a
Muhammad Nabeel <47273004+NabeelJaan@users.noreply.github.com>
2023-01-03 21:55:14 +0500

    code editor done

--- components/home/languages.jsx
+++ components/home/languages.jsx
@@ -14,7 +14,7 @@ const Languages = () => {
                         <p className="text-[19px] text-center text-black font-normal">The language for building web pages</p>
 
                         <div className="btn____link my-7">
-                            
+
                             <div className="button___link bg-[#04AA6D] inline-flex items-center justify-center w-[200px] h-[43px] border border-[#04AA6D] rounded-full mr-5">
                                 <Link href="/" className="text-[18px] text-white font-normal">
                                     Learn HTML
@@ -46,9 +46,17 @@ const Languages = () => {
                     <div className="html-example p-[16px] bg-[#E7E9EB] max-w-[615px] rounded-md">
                         <h3 className="text-[24px] text-black font-semibold uppercase">html example:</h3>
 
-                        <div className="example bg-white w-[580px] h-[300px] my-4">
-
+                        <div className="example bg-white w-[580px] h-[300px] my-4 border-l-[6px] border-red-100">
+                            <div className=""><span>&lt;</span>!DOCTYPE html<span>&gt;</span></div>
+                            <div className=""><span>&lt;</span>html<span>&gt;</span></div>
+                            <div className=""><span>&lt;</span>!DOCTYPE html<span>&gt;</span></div>
+                            <div className=""><span>&lt;</span>!DOCTYPE html<span>&gt;</span></div>
+                            <div className=""><span>&lt;</span>!DOCTYPE html<span>&gt;</span></div>
+                            <div className=""><span>&lt;</span>!DOCTYPE html<span>&gt;</span></div>
+                            <div className=""><span>&lt;</span>!DOCTYPE html<span>&gt;</span></div>
+                            <div className=""><span>&lt;</span>html<span>&gt;</span></div>
                         </div>
+
                         <div className="button___link bg-[#04AA6D] inline-flex items-center justify-center w-[200px] h-[43px] border border-[#04AA6D] rounded-full mr-5">
                             <Link href="/" className="text-[18px] text-white font-normal">
                                 Try it Yourself
